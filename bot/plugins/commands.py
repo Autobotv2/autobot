696c50f3c2b6777aa9e07f135d2e1fff7849f1e9
Autobotv2 <87906220+Autobotv2@users.noreply.github.com>
2021-07-25 13:53:06 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'JOIN', url="https://t.me/malayalam_cinema_group"
                                 )
                         ]
                     ]
